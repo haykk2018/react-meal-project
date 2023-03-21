60e69486088c7fc2680fbf849d6c7ba95e0ebb5d
hayk <haykk2018@gmail.com>
2023-03-21 15:04:11 +0400

    REACT ROUTER

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import {Header} from "./components/Header";
 import Main from "./components/Main";
 import {Footer} from "./components/Footer";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {About} from "./components/pages/About";
+import {Contact} from "./components/pages/Contact";
+import {NotFound} from "./components/pages/NotFound";
 
 function App() {
     return <>
-        <Header/>
-        <Main/>
-        <Footer/>
+        <Router>
+            <Header/>
+
+            <Routes>
+                <Route exact path='/' element={<Main/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/contacts' element={<Contact/>}/>
+                <Route path='*' element={<NotFound/>}/>
+            </Routes>
+            <Footer/>
+        </Router>
     </>
 }
 
