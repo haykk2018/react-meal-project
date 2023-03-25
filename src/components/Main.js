95ad30cfe9560b9ed93137acadd0889d775f8a00
hayk <haykk2018@gmail.com>
2023-03-25 13:34:05 +0400

    filtering functionality without ability to take value from link and filtering by that value

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,26 +2,28 @@ import {useEffect, useState} from "react";
 import {CategoryList} from "./CategoryList";
 import {getAllCategories} from "../api";
 import {Search} from "./Search";
+import {useLocation} from "react-router-dom";
 
 const Main = () => {
 
     const [catalog, setCatalog] = useState([]);
     //it's need because if we save filtered data in catalog, next filtering it will do from already filtered data
     const [filteredCatalog, setFilteredCatalog] = useState([]);
+    const location = useLocation();
 
     useEffect(() => {
         getAllCategories().then((data) => {
             setCatalog(data.categories);
-            filter('');
+            const nameFromLink = location["search"].split('=')[1].toLowerCase();
+            // we pass here names as argument because it can't take catalog from useEffect hook
+            filter(nameFromLink, data.categories);
         });
     }, []);
 
-    const filter = (name) => {
-
+    const filter = (name, names = catalog) => {
         setFilteredCatalog(
-            catalog.filter((item) => item.strCategory.toLowerCase().includes(name.toLowerCase()))
-        )
-
+            names.filter((item) => item.strCategory.toLowerCase().includes(name.toLowerCase()))
+        );
     }
 
     return <>
